8a21e8e3591b6eb7b1483fc6ac882b2862837139
Jung Hyun, Nam <rkttu@rkttu.com>
2025-10-03 18:29:55 +0900

    오디오/비디오 입출력은 끄도록 설정

--- src/TableClothLite.Shared/Models/SandboxConfig.cs
+++ src/TableClothLite.Shared/Models/SandboxConfig.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.VisualBasic;
-
 namespace TableClothLite.Shared.Models;
 
 public sealed record class SandboxConfig
 {
     public bool EnableNetworking { get; init; } = true;
-    public bool EnableAudioInput { get; init; } = true;
-    public bool EnableVideoInput { get; init; } = true;
+    public bool EnableAudioInput { get; init; } = false;
+    public bool EnableVideoInput { get; init; } = false;
     public bool EnablePrinterRedirection { get; init; } = true;
     public bool EnableClipboardRedirection { get; init; } = true;
     public string OpenRouterModel { get; init; } = Constants.DefaultOpenRouterModel;
