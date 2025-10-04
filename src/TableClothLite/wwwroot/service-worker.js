ade4b751636961675e4eccc6f56bd4c52cd6bbf7
Jung Hyun, Nam <rkttu@rkttu.com>
2025-10-04 19:29:20 +0900

    서비스 워커에서 메시지 이벤트 핸들러 제거
    `service-worker.js` 파일에서 `self.addEventListener('message', event => onMessage(event));` 라인이 제거되었습니다. 이로 인해 서비스 워커가 더 이상 'message' 이벤트를 수신하고 처리하지 않게 되었습니다. 이는 클라이언트와 서비스 워커 간의 통신 기능이 제거되었음을 의미합니다.

--- src/TableClothLite/wwwroot/service-worker.js
+++ src/TableClothLite/wwwroot/service-worker.js
@@ -5,7 +5,6 @@ self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
-self.addEventListener('message', event => onMessage(event));
 
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
