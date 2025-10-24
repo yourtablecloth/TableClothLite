2e0988fc50959c5de1e71177f858cb438599d9b9
rkttu <rkttu@rkttu.com>
2025-10-24 07:40:38 +0000

    Deploying to gh-pages from @ yourtablecloth/TableClothLite@7c75867ca17bd46caa8f54d3aa73e76feb6140c2 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: OsNW1cEf */
+/* Manifest version: Slpvn0MB */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
